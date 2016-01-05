e92324b4737e554a02ee993ce21dead238043fe5
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-05 13:58:59 -0500

    iterate hash'

--- playlist.js
+++ playlist.js
@@ -17,3 +17,9 @@ function changePlaylistSong(playlist, song, artist){
   playlist[artist] = song;
   return playlist;
 }
+
+function readPlaylist(playlist){
+  for (var key in playlist){
+    console.log("The song " + playlist[key] + " by " + key + " is in your playlist");
+  }
+}
