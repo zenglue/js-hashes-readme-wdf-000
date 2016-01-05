0888cb23d2eb0f0f32631aacf1958780454bc7cf
vicfriedman <victoria.m.friedman@gmail.com>
2016-01-05 09:39:07 -0500

    solution

--- playlist.js
+++ playlist.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// var myPlaylist = {"taylor swift": "out of the woods"}
+
+function addToPlaylist(playlist, song, artist){
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
+
+
+function changePlaylistSong(playlist, song, artist){
+  playlist[artist] = song;
+  return playlist;
+}
